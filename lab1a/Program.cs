bead0e923d50a2a1b37cca470e1b1f7fb2f55004
DanSoW <67583913+DanSoW@users.noreply.github.com>
2022-10-25 08:39:14 +0800

    feat(): init

---
+++ lab1a/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Program
+{
+    internal class Program
+    {
+        static int N = 1000;             // Общее количество значений массива
+        static int[]? a = null;          // Значения массива
+        static int n;                    // Количество запущенных потоков
+        static int[]? returns = null;    // Возвращаемые значения
+
+        /// <summary>
+        /// Функция выполнения для потока
+        /// </summary>
+        /// <param name="param">Параметры функции</param>
+        /// <returns>Сумма чётных элементов массива</returns>
+        static int ThreadFunc(int param)
+        {
+            int nt, beg, h, end;
+            int sum = 0;
+
+            nt = param;
+            h = N / n;
+
+            // Вычисление начала и конца обхода массива
+            beg = h * nt;
+            end = beg + h;
+
+            if (nt == (n - 1))
+            {
+                end = N;
+            }
+
+            Console.WriteLine(
+                "Поток {0} начало {1} конец {2}",
+                nt, beg, end
+            );
+
+            for(int i = beg; i < end; i++)
+            {
+                if (a[i] % 2 != 0)
+                {
+                    sum += a[i];
+                }
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Точка входа в приложение
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Введите количество потоков, используемых для вычислений в текущем процессе: ");
+            while (true)
+            {
+                if ((Int32.TryParse(Console.ReadLine(), out n)) && (n > 0))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода! Необходимо ввести натуральное число отличное от нуля!");
+                }
+            }
+
+            Random rand = new Random();
+            N = rand.Next(100000, 1000000);
+            a = new int[N];
+
+            // Объект мониторинга времени выполнения
+            Stopwatch sWatch = new Stopwatch();
+
+            // Массив потоков
+            Thread[] threads = new Thread[n];
+
+            int rez = 0, s = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rand.Next(100, 10000);
+            }
+
+            // Начало отсчёта времени выполнения операций
+            sWatch.Start();
+
+            returns = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                // Необходима локальная переменная, чтобы
+                // потоки не ссылались на переменную i в цикле
+                int tmp = i;
+                returns[tmp] = 0;
+
+                threads[i] = new Thread(() =>
+                {
+                    // Возвращаемое значение для определённого потока
+                    returns[tmp] = ThreadFunc(tmp);
+                });
+
+                // Запуск потока
+                threads[i].Start();
+            }
+
+            // Ожидание всех потоков до конца их выполнения
+            for (int i = 0; i < n; i++)
+            {
+                threads[i].Join();
+                rez += returns[i];
+
+                Console.WriteLine("Сумма {0}-го потока: {1}", i, returns[i]);
+            }
+
+            sWatch.Stop();
+            Console.WriteLine("Затраченное время в миллисекундах: " + sWatch.ElapsedMilliseconds.ToString());
+            Console.WriteLine("Сумма чётных элементов массива {0} потоками = {1}", n, rez);
+            sWatch.Reset();
+
+            sWatch.Start();
+            for(int i = 0; i < N; i++)
+            {
+                if (a[i] % 2 != 0)
+                {
+                    s += a[i];
+                }
+            }
+            sWatch.Stop();
+            Console.WriteLine("Затраченное время в миллисекундах: " + sWatch.ElapsedMilliseconds.ToString());
+            Console.WriteLine("Сумма чётных элементов массива без потоков = {0}", s);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab1b/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Program
+{
+    internal class Program
+    {
+        static Mutex mut = new Mutex();
+        static public Stream outStream;
+        static public Stream inStream;
+        public static AnonymousPipeServerStream pipeServer;
+        public static AnonymousPipeClientStream pipeClient;
+        static public string pipeHandle;
+        static int mode = 0;
+        static public int i = 0;
+        public static float sredn = 0, dlina = 0;
+        static bool stop = false;
+
+        public static void Second()
+        {
+            pipeClient = new AnonymousPipeClientStream(PipeDirection.In, pipeHandle);
+
+            string? buffer = "";
+            StreamReader binReader = new StreamReader(pipeClient);
+            while (!stop)
+            {
+                buffer = binReader.ReadLine();
+                mut.WaitOne();
+                int tmp = mode;
+                Console.WriteLine("Message read ({0}): {1}",DateTime.Now, buffer);
+                if (tmp == 1)
+                {
+                    Console.WriteLine("Обработка строки");
+                    if (buffer != null)
+                    {
+                        string[] msgRcv = buffer.Split(';');
+                        int index = msgRcv[1].IndexOf(msgRcv[0]);
+
+                        if(index >= 0)
+                        {
+                            string result = msgRcv[1].Substring(0, (index + 1))
+                                + msgRcv[2]
+                                + msgRcv[1].Substring((index + 1));
+                            Console.WriteLine("Результат: " + result);
+                        }else
+                        {
+                            Console.WriteLine("Данный символ отсутствует в целевой строке!");
+                        }
+                    }
+                }
+                else if (tmp == 2)
+                {
+                    Console.WriteLine("Обработка чисел ... ");
+                    int counter = 0;
+
+                    if (buffer != null)
+                    {
+                        string[] msgRcv = buffer.Split(' ');
+                        int[] values = new int[msgRcv.Length];
+
+                        for (int j = 0; j < values.Length; j++)
+                        {
+                            if (int.TryParse(msgRcv[j], out _))
+                            {
+                                values[j] = Convert.ToInt32(msgRcv[j]);
+                                if ((values[j] > 0) && (j % 2 != 0))
+                                {
+                                    counter++;
+                                }
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Количество положительных чисел в массиве находящиеся на чётных индексах равно {0}", counter);
+                }
+                else
+                {
+                    Console.WriteLine("Shutting down!");
+                }
+                mut.ReleaseMutex();
+            }
+            binReader.Close();
+        }
+
+        public static void Main()
+        {
+            Thread th2 = new Thread(() => Second()); // Создание нового потока
+            pipeServer = new AnonymousPipeServerStream(PipeDirection.Out);
+            outStream = pipeServer;
+            pipeHandle = pipeServer.GetClientHandleAsString();
+            th2.Start();
+            StreamWriter writer = new StreamWriter(outStream);
+            writer.AutoFlush = true;
+
+            while (!stop)
+            {
+                Console.WriteLine("Введите цифру 1 для того что бы выполнить задание с буквенной строкой");
+                Console.WriteLine("Введите цифру 2 для того что бы выполнить задание с массивом");
+                Console.WriteLine("Введите цифру 3 для того что бы завершить выполнение программы");
+
+                int com;
+                while (true)
+                {
+                    if ((Int32.TryParse(Console.ReadLine(), out com)) && ((com == 1) || (com == 2) || (com == 3)))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка ввода! Необходимо ввести натуральное число в диапазоне [1; 3]");
+                    }
+                }
+
+                mut.WaitOne();
+                mode = com;
+                switch (com)
+                {
+                    case 1:
+                        Console.WriteLine("Введите буквенную строку 1");
+                        string? stroka1 = Console.ReadLine();
+
+                        Console.WriteLine("Введите буквенную строку 2");
+                        string? stroka2 = Console.ReadLine();
+
+                        Console.WriteLine("Введите символ");
+                        char? symbol1 = Console.ReadLine()[0];
+
+                        string tmpStr = symbol1 + ";" + stroka1 + ";" + stroka2;
+                        writer.WriteLine(tmpStr);
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите элементы массива через пробел");
+                        string? stroka3 = Console.ReadLine();
+                        writer.WriteLine(stroka3);
+                        break;
+                    case 3:
+                        stop = true;
+                        break;
+                }
+                mut.ReleaseMutex();
+            }
+            writer.Close();
+
+            th2.Join();
+        }
+    }
+}
